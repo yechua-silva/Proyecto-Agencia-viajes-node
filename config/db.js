eaeca6b24e3cb1e8a9c89bada436b6e1a6f99c90
yechua-silva <yechua_silva@outlook.cl>
2023-06-22 19:22:43 -0400

    modificar puerto para deployment

--- config/db.js
+++ config/db.js
@@ -4,12 +4,11 @@ import dotenv from "dotenv/config";
 // Configuramos dotenv, lee automaticamente el archivo.env
 // dotenv.config();
 
-console.log(process.env.DB_HOST);
 
 // EL primer parametro es el nombre de la base de datos, segundo el nombre de usuario y el ultimo la contraseña
 const db = new Sequelize( process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
-    port: 3306,
+    port: process.env.DB_PORT,
     dialect:'mysql',
     define: {
         timestamps: false
