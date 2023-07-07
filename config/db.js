76a35fadc47fe7d9e34f806e68a6866f18fb44ff
yechua-silva <yechua_silva@outlook.cl>
2023-07-07 19:32:58 -0400

    Migracion de base de datos de MySql a Postgre

--- config/db.js
+++ config/db.js
@@ -9,7 +9,7 @@ import dotenv from "dotenv/config";
 const db = new Sequelize( process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect:'mysql',
+    dialect:'postgres',
     define: {
         timestamps: false
     },
